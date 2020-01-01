839f77cd9e491a5ba91f2909f7d1d85eaee6a510
github username <github email address>
2019-12-31 19:00:00 -0500

    homepage

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,25 +1,74 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'
+import Slider from 'react-slick'
 
 export class HomeContainer extends Component {
     render() {
+        let settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          };
         return (
-            // <div className="container ui">
-            //     <h2 className="ui center aligned icon header ui orange message">
-            //         <i className="basketball ball icon"></i>
-            //         BS NBA Database
-            //     </h2>
-            // </div>
-            <div class="ui inverted vertical masthead center aligned segment">
-
-                <div class="ui text container">
-                    <h1 class="ui inverted header">
-                        Imagine-a-Company
-                    </h1>
-                    <h2>Do whatever you want when you want to.</h2>
-                    <div class="ui huge primary button">Get Started <i class="right arrow icon"></i></div>
+            <div className="ui grid">
+                <div className="container ui">
+                    <h2 className="ui center aligned icon header ui orange message">
+                        <i className="basketball ball icon"/>
+                        BS NBA Database
+                    </h2>
                 </div>
-
-            </div>
+                <div class="ui hidden divider"></div>
+                <div class="ui hidden divider"></div>
+                <div class="ui hidden divider"></div>
+                <div className="ui items">
+                    <div className="item">
+                        <img className="ui large rounded image" src="http://i.imgur.com/kuyqOdO.png"/>
+                        <div className="content">
+                            <br></br>
+                            <h2 className="header">Player Profiles</h2>
+                            <br></br>
+                            <div className="description">
+                                <p>View individual player profile and statistics including per-game-averages and season totals.</p>
+                                <p>Includes every player that has played in the NBA from 1979 to 2019.</p>
+                                <br></br>
+                                <NavLink to="/players">
+                                    <button className="ui inverted orange button">Search for Player</button>
+                                </NavLink>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br></br>
+                <div class="ui divider"></div>
+                <br></br>
+                <div className="ui items">
+                    <div className="item">
+                        <div className="content">
+                            <br></br>
+                            <h2 className="header">Player Profiles</h2>
+                            <br></br>
+                            <div className="description">
+                                <p>View individual player profile and statistics including per-game-averages and season totals.</p>
+                                <p>Includes every player that has played in the NBA from 1979 to 2019.</p>
+                                <br></br>
+                                <NavLink to="/teams">
+                                    <button className="ui inverted orange button">Team Page</button>
+                                </NavLink>
+                            </div>
+                        </div>
+                        <Slider {...settings}>
+                            <div>
+                                <img className="ui medium rounded image" src="http://i.imgur.com/TRxXXyU.png"/>
+                            </div>
+                            <div>
+                                <img className="ui medium rounded image" src="http://i.imgur.com/TRxXXyU.png"/>
+                            </div>
+                        </Slider>
+                    </div>
+                </div>
+            </div>   
         );
     }
 }
