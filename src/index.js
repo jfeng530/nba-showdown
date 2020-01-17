68c171ec6d90b6a669c1eafa71bf36f4edb3468a
github username <github email address>
2020-01-17 13:28:13 -0500

    remove theme

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers/reducer'
@@ -12,9 +13,9 @@ const store = createStore(reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(<Provider store={store}>
-                  <Router>
+                  <HashRouter basename='/'>
                     <App />
-                  </Router>
+                  </HashRouter>
                 </Provider>,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
