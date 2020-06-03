c5393859133631fc813f9fedd952e142d4c4e9eb
Jacky Feng <jackyfeng530@yahoo.com>
2020-06-02 22:18:37 -0400

    Updates

---
+++ precache-manifest.c1584471a4fa11f4493b30344000e1be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa28b2d756ec97ddea648e0472ada6d8",
+    "url": "/nba-showdown/index.html"
+  },
+  {
+    "revision": "ea043b97afba72ea98eb",
+    "url": "/nba-showdown/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c6b2377444d9d2c1ab35",
+    "url": "/nba-showdown/static/css/main.64f6cf45.chunk.css"
+  },
+  {
+    "revision": "ea043b97afba72ea98eb",
+    "url": "/nba-showdown/static/js/2.3321697c.chunk.js"
+  },
+  {
+    "revision": "c6b2377444d9d2c1ab35",
+    "url": "/nba-showdown/static/js/main.c9776b53.chunk.js"
+  },
+  {
+    "revision": "02536ae8b2cf6427b580",
+    "url": "/nba-showdown/static/js/runtime-main.14a53e40.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nba-showdown/precache-manifest.c2e7cc1a389ad79095a6abc12033e8b4.js"
+  "/nba-showdown/precache-manifest.c1584471a4fa11f4493b30344000e1be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nba-showdown/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
